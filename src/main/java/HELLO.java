e608ac167d3483373aaffbe172eed5c41f751e2b
metinerden <summmerdate@gmail.com>
2021-02-19 15:55:45 +0200

    fleetgru2

--- src/main/java/HELLO.java
+++ src/main/java/HELLO.java
@@ -2,5 +2,6 @@ public class HELLO {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("FLEETGRU");
+        System.out.println("fleet2");
     }
 }
